36575476172e7c456eacc14af57f04f034f5b211
ExtremeSoul <clegutko@gmail.com>
2017-11-06 10:59:13 +0100

    add employee

--- src/pl/sdacademy/java7krk/Main.java
+++ src/pl/sdacademy/java7krk/Main.java
@@ -7,12 +7,15 @@ public class Main {
         Employee student = new Student(1450.27);
         Employee temporary = new TemporaryEmployee(2000.43);
         Employee full = new FullTimeEmployee(3000.40);
+//        Employee full1 = new FullTimeEmployee(3046.32);
 
         company.addEmployee(student);
         company.addEmployee(temporary);
         company.addEmployee(full);
+//        company.addEmployee(full1);
 
         System.out.println(String.format("%.2f", company.sumOfSalaries()));
         System.out.println(String.format("%.2f", company.averageOfSalaries()));
+
     }
 }
